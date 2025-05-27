155f18989cd040da8a8b416e54caecf0e7301d02
antonino63 <antoninosantagata@gmail.com>
2025-05-27 12:12:34 +0200

    Aumentata temperatura a 0.6 per tono più amichevole

--- chat.py
+++ chat.py
@@ -47,7 +47,8 @@ def chat():
 
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=messages
+            messages=messages,
+            temperature=0.6
         )
 
         raw_reply = response.choices[0].message["content"]
